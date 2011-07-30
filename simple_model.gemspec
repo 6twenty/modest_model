cb98091ca945230b48a26603614c037dc8605fa0
Mike Fulcher <mike@plan9design.co.uk>
2011-07-30 16:53:13 +0100

    Backtracking. Tests fail without the dummy Rails app :p

--- simple_model.gemspec
+++ simple_model.gemspec
@@ -9,4 +9,5 @@ Gem::Specification.new do |s|
   
   s.add_dependency "activemodel", "~> 3"
   s.add_development_dependency "rails", "~> 3"
+  s.add_development_dependency "sqlite3"
 end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '515514ee6d1b2d0d850379bf295584a36f86da2c9901afe0f2a82b8d7fd3240f209dcd37b12ac2376a0e3e03ba56b7f9d2cb7a1548463d8c844dd39a7535b98d'
