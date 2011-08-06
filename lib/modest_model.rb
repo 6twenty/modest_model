d347c3b38b19d8b013702a5f8447c28993d7fd7d
Mike Fulcher <6twenty@gmail.com>
2011-08-06 10:18:05 +0100

    Add a shortcut method to create ModestModel classes on the fly given a hash of attributes

--- lib/modest_model.rb
+++ lib/modest_model.rb
@@ -9,4 +9,11 @@ module ModestModel
   autoload :Validators,         File.expand_path('../modest_model/validators', __FILE__)
   autoload :Relation,           File.expand_path('../modest_model/relation', __FILE__)
   autoload :HasManyCollection,  File.expand_path('../modest_model/has_many_collection', __FILE__)
+  
+  # Build a basic ModestModel by passing in a hash of key/value attributes
+  def self.new(attributes={})
+    klass = const_set("Generic#{Object.new.object_id}", Class.new(Base))
+    klass.attributes *attributes.keys.map(&:to_sym)
+    klass.new(attributes)
+  end
 end
