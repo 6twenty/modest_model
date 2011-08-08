23ae24b1f52cfc94bd3afb15f3d56f07316c18eb
Mike Fulcher <mike@plan9design.co.uk>
2011-08-08 17:38:39 +0100

    Remove the basic version from the Advanced branch

--- lib/modest_model.rb
+++ lib/modest_model.rb
@@ -9,10 +9,4 @@ module ModestModel
   autoload :Validators,         File.expand_path('../modest_model/validators', __FILE__)
   autoload :Relation,           File.expand_path('../modest_model/relation', __FILE__)
   autoload :HasManyCollection,  File.expand_path('../modest_model/has_many_collection', __FILE__)
-  autoload :Generic,            File.expand_path('../modest_model/generic', __FILE__)
-  
-  # Build a basic ModestModel by passing in a hash of key/value attributes
-  def self.new(attrs={})
-    const_set("Generic#{Object.new.object_id}", Class.new(Generic)).new(attrs)
-  end
 end
